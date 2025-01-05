a4768574e9581c19907dcf9dd11eb846eb0d75e7
Oyelola Oluwafemi Emmanuel <36579635+wafemi999@users.noreply.github.com>
2025-01-05 23:40:44 +0100

    Update Arithmetic ops.py

--- Arithmetic ops.py
+++ Arithmetic ops.py
@@ -1,17 +1,18 @@
 import math
 
-print(10//3) # floor divison
+print(10//3) #floor divison
 
 print(10**3)
 
 x=10
-x += 3 # ehanced or augmented assignment operator
+x += 3 #ehanced or augmented assignment operator
 print(x)
 
 # operator precedence:
 #pEMDAS:parenthesis, exponentiation, mult, division, addition & substaction
 
 # MATH FUNCTION, import module math
+
 print(round(2.9))
 print(math.floor(2.9))
 print(math.ceil(2.9))
